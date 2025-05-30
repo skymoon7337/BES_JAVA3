b9c85f0d8ea76d7835a2de4482191d7ca8da6ca5
skymoon7337 <skymoon7337@gmail.com>
2025-05-30 14:45:32 +0900

    First

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 25;
+        System.out.println(age);
+
+        age = 26;
+        System.out.println(age);
+
+        boolean Story = true;
+        System.out.println(Story);
+
+        double salary = 5000.123456789;
+        System.out.println(salary);
+
+        char alphabet = 'a';
+        System.out.println(alphabet);
+
+        String name = "문하늘";
+        System.out.println(name);
+        System.out.println(name.length());
+
+    }
+}
